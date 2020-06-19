dcf3c4e1cf2c02d2a9fa8982efb17329024893b5
jiae <hirlawldo413@gmail.com>
2020-06-19 23:35:23 +0900

    [chapter3] operator

---
+++ src/chapter3/operator/OperatorEx3.java
@@ -0,0 +1,12 @@
+package operator;
+
+public class OperatorEx3 {
+
+	public static void main(String[] args) {
+		int score = 100;
+		System.out.println(++score);
+		score = score + 1;
+		score += 1;
+	}
+
+}
