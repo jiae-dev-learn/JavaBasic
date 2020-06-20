787c80f24b6f0ae55d905a7bf89d04e753315c08
jiae <hirlawldo413@gmail.com>
2020-06-20 17:58:04 +0900

    [chapter3] change package

---
+++ src/chapter3/operator/OperatorEx2.java
@@ -0,0 +1,14 @@
+package chapter3.operator;
+
+public class OperatorEx2 {
+
+	public static void main(String[] args) {
+		int num1 = 5%3;
+		System.out.println(num1);
+		
+		int num2 = 10;
+		num2 += 2;
+		System.out.println(num2);
+	}
+
+}
---
+++ src/chapter3/operator/OperatorEx3.java
@@ -0,0 +1,12 @@
+package chapter3.operator;
+
+public class OperatorEx3 {
+
+	public static void main(String[] args) {
+		int score = 100;
+		System.out.println(++score);
+		score = score + 1;
+		score += 1;
+	}
+
+}
